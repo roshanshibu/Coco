af9a3b67c71efd7ed7fb45d19ac60967da54b651
roshan <roshanshibu97@gmail.com>
2023-02-24 21:30:35 +0100

    playing around with card library

--- src/components/chorus/Chorus.jsx
+++ src/components/chorus/Chorus.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import TinderCard from 'react-tinder-card'
+import "./Chorus.css";
+
+const onSwipe = (direction) => {
+    console.log('You swiped: ' + direction)
+}
+  
+const onCardLeftScreen = (myIdentifier) => {
+    console.log(myIdentifier + ' left the screen')
+}
 
 const Chorus = () => {
     return(
-        <p>Chorus Page</p>
+        <>
+            <p>Chorus Page</p>
+            <div className='cardsContainer'>
+                <TinderCard className='chorusCard' onSwipe={onSwipe} 
+                    onCardLeftScreen={() => onCardLeftScreen('fooBar')} 
+                    preventSwipe={['down']}>
+                    <div>
+                        <p className='songName'>Song Name</p>
+                        <p className='artistName'>Artist</p>
+                    </div>
+                </TinderCard>
+            </div>
+        </>
     )
 }
 
