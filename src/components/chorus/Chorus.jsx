a155917de651043da243f4b9b5dc695e6ceff6dd
roshan <roshanshibu97@gmail.com>
2023-02-25 18:03:11 +0100

    added infinite chorus stack

--- src/components/chorus/Chorus.jsx
+++ src/components/chorus/Chorus.jsx
@@ -11,9 +11,10 @@ const ChorusCard = (props) => {
     }
 
     const onSwipe = async(direction) => {
-        console.log('card swiped to ' + direction)
+        // console.log('card swiped to ' + direction)
         await delay()
         setSwipedOut(true)
+        props.reportSwipe(direction, props.markerCard)
     }
 
     const onCardLeftScreen = (songName) => {
@@ -21,46 +22,81 @@ const ChorusCard = (props) => {
     }
 
     useEffect(() => {
-        if(document.getElementsByClassName('chorusCard')[0]){
+        console.log(document.getElementsByClassName('chorusCard')[document.getElementsByClassName('chorusCard').length - 1].firstChild.firstChild.textContent);
+        if(document.getElementsByClassName('chorusCard')[document.getElementsByClassName('chorusCard').length - 1]){
+            console.log(document.getElementsByClassName('chorusCard')[document.getElementsByClassName('chorusCard').length - 1])
             var observer = new MutationObserver(function(mutations) {
                 mutations.forEach(function(mutationRecord) {
-                    if(document.getElementsByClassName('chorusCard')[0])
-                        console.log(document.getElementsByClassName('chorusCard')[0].style.transform.replace(/translate3d|px|\(|\)/gi, '').split(','));
+                    if(document.getElementsByClassName('chorusCard')[document.getElementsByClassName('chorusCard').length - 1])
+                        console.log(document.getElementsByClassName('chorusCard')[document.getElementsByClassName('chorusCard').length - 1].style.transform.replace(/translate3d|px|\(|\)/gi, '').split(','));          
                 });    
             });
             
-            var target = document.getElementsByClassName('chorusCard')[0];
+            var target = document.getElementsByClassName('chorusCard')[document.getElementsByClassName('chorusCard').length - 1];
             observer.observe(target, { attributes : true, attributeFilter : ['style'] });
         }
     });
 
     return(
-        <TinderCard className={swipedOut ? 'gone' : 'chorusCard'} 
-            onSwipe={onSwipe}
-            onCardLeftScreen={() => onCardLeftScreen(props.songName)} 
-            preventSwipe={['down']}
-            swipeRequirementType={'position'}
-            swipeThreshold={150}
-            >
-            <div style={{backgroundColor: props.color}} className="cardDetails">
-                <p className='songName'>{props.songName}</p>
-                <p className='artistName'>{props.artist}</p>
-            </div>
-        </TinderCard>
+            <TinderCard className={swipedOut? 'gone':'chorusCard'} 
+                onSwipe={onSwipe}
+                onCardLeftScreen={() => onCardLeftScreen(props.songName)} 
+                preventSwipe={['down']}
+                swipeRequirementType={'position'}
+                swipeThreshold={150}
+                >
+                <div style={{backgroundColor: props.color}} className="cardDetails">
+                    <p className='songName'>{props.songName}</p>
+                    <p className='artistName'>{props.artist}</p>
+                </div>
+            </TinderCard>
     )
 }
 
+const ChorusCardStack = () => {
+
+    const getNext5Cards = async() => {
+        let songname = "Song " + Math.random()
+        let artist = "Artist - " + Math.random()
+        let color = (Math.floor(Date.now() / 1000) % 2 == 0 ? "pink" : "yellow")
+        return ([<ChorusCard songName={"LOADING"} artist={"LOADING"} color={color} reportSwipe={reportSwipe} key={Math.random()*1000} />,
+        <ChorusCard songName={("song "+Math.random()*1000)} artist={artist} color={color} reportSwipe={reportSwipe} key={Math.random()*1000} markerCard={true} />,
+        <ChorusCard songName={("song "+Math.random()*1000)} artist={artist} color={color} reportSwipe={reportSwipe} key={Math.random()*1000} />,
+        <ChorusCard songName={("song "+Math.random()*1000)} artist={artist} color={color} reportSwipe={reportSwipe} key={Math.random()*1000} />,
+        <ChorusCard songName={("song "+Math.random()*1000)} artist={artist} color={color} reportSwipe={reportSwipe} key={Math.random()*1000} />
+        ])
+    }
+        
+    const reportSwipe = (direction, marker) => {
+        console.log("card swiped to the "+direction)
+        if(marker){
+            let newCard = getNext5Cards()
+            console.log("does not match")
+            newCard.then((nextCard) => {
+                setDynamicStack([nextCard, ...dynamicStack])
+            })
+        }
+    }
+
+    useEffect(()=>{
+        console.log(dynamicStack)
+    })
+    
+    let topCard = (<ChorusCard songName="Song1" artist="Artist1" color="red" reportSwipe={reportSwipe} key={0} markerCard={true} />)
+    let nextCard = (<ChorusCard songName="Song2" artist="Artist2" color="green" reportSwipe={reportSwipe} key={1}  />)
+    const [dynamicStack, setDynamicStack] = useState([nextCard, topCard]);
+    return(
+        <>
+            {dynamicStack}
+        </>
+    )
+}
 const Chorus = () => {
     return(
         <>
             <p>Chorus Page</p>
             <div className='cardsContainer'>
-                <ChorusCard songName="Hello" artist="Adele" color="red" />
-                <ChorusCard songName="Running Out" artist="Astrid S" color="blue" />
-                <ChorusCard songName="Song 3" artist="Artist A" color="green" />
-                <ChorusCard songName="Song 4" artist="Artist B" color="yellow" />
-                <ChorusCard songName="Song 5" artist="Artist C" color="orange" />
-                <ChorusCard songName="Song 6" artist="Artist D" color="pink" />
+                <ChorusCardStack />
             </div>
         </>
     )
